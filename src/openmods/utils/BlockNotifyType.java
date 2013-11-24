9e4a911c0d67a2d2a3e748beb2cfdafc838d7672
bspkrs <bspkrs@gmail.com>
2013-11-23 22:17:00 -0500

    Added BlockNotifyType.java and utilized it in Coord.java

---
+++ src/openmods/utils/BlockNotifyType.java
@@ -0,0 +1,14 @@
+package openmods.utils;
+
+/*
+ * @author: bspkrs
+ * @desc: provides constants for the types of block notifications possible in World.setBlock()
+ */
+
+public class BlockNotifyType{
+    public static final int NONE          = 0;
+    public static final int COMPARATOR    = 1;
+    public static final int CLIENT_SERVER = 2;
+    public static final int ALL           = 3;
+    public static final int CLIENT_ONLY   = 4;
+}
