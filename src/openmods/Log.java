04a21ad1ea29b6c82150b66ba96b91733d017be6
Bartek Bok <bartek.bok@gmail.com>
2013-12-03 17:38:04 +0100

    Fancy log

--- src/openmods/Log.java
+++ src/openmods/Log.java
@@ -8,8 +8,52 @@ public final class Log {
 
 	public static Logger logger;
 
+	private static final Throwable stackInfo = new Throwable();
+
+	private static String getLogLocation(Throwable t) {
+		// first element is always log function
+
+		// maybe faster but definitely unsafe implementation:
+		// JavaLangAccess access = SharedSecrets.getJavaLangAccess();
+		// if (access.getStackTraceDepth(t) < 2) return "";
+		// final StackTraceElement caller = access.getStackTraceElement(t, 1);
+
+		final StackTraceElement[] stack = t.getStackTrace();
+		if (stack.length < 2) return "";
+		final StackTraceElement caller = stack[1];
+		return caller.getClassName() + "." + caller.getMethodName() + "(" + caller.getFileName() + ":" + caller.getLineNumber() + "): ";
+	}
+
+	private static void logWithCaller(Throwable callerStack, Level level, String format, Object... data) {
+		logger.log(level, getLogLocation(callerStack) + String.format(format, data));
+	}
+
 	public static void log(Level level, String format, Object... data) {
-		logger.log(level, String.format(format, data));
+		logWithCaller(stackInfo.fillInStackTrace(), level, format, data);
+	}
+
+	public static void severe(String format, Object... data) {
+		logWithCaller(stackInfo.fillInStackTrace(), Level.SEVERE, format, data);
+	}
+
+	public static void warn(String format, Object... data) {
+		logWithCaller(stackInfo.fillInStackTrace(), Level.WARNING, format, data);
+	}
+
+	public static void info(String format, Object... data) {
+		logWithCaller(stackInfo.fillInStackTrace(), Level.INFO, format, data);
+	}
+
+	public static void fine(String format, Object... data) {
+		logWithCaller(stackInfo.fillInStackTrace(), Level.FINE, format, data);
+	}
+
+	public static void finer(String format, Object... data) {
+		logWithCaller(stackInfo.fillInStackTrace(), Level.FINER, format, data);
+	}
+
+	public static void finest(String format, Object... data) {
+		logWithCaller(stackInfo.fillInStackTrace(), Level.FINEST, format, data);
 	}
 
 	public static void log(Level level, Throwable ex, String format, Object... data) {
@@ -20,31 +64,7 @@ public final class Log {
 		log(Level.SEVERE, ex, format, data);
 	}
 
-	public static void severe(String format, Object... data) {
-		log(Level.SEVERE, format, data);
-	}
-
 	public static void warn(Throwable ex, String format, Object... data) {
 		log(Level.WARNING, ex, format, data);
-	}
-
-	public static void warn(String format, Object... data) {
-		log(Level.WARNING, format, data);
-	}
-
-	public static void info(String format, Object... data) {
-		log(Level.INFO, format, data);
-	}
-
-	public static void fine(String format, Object... data) {
-		log(Level.FINE, format, data);
-	}
-
-	public static void finer(String format, Object... data) {
-		log(Level.FINER, format, data);
-	}
-
-	public static void finest(String format, Object... data) {
-		log(Level.FINEST, format, data);
 	}
 }
