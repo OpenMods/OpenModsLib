7f1dd3ec229be1c662a547b0f46d5e519018b474
Mikee Franklin <mikeefranklin@gmail.com>
2014-02-11 22:00:44 +0000

    meh. sound wont work. not sure why

---
+++ src/openmods/sounds/SoundLoader.java
@@ -0,0 +1,22 @@
+package openmods.sounds;
+
+import net.minecraftforge.client.event.sound.SoundLoadEvent;
+import net.minecraftforge.event.ForgeSubscribe;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+
+public class SoundLoader {
+
+	private final static String[] soundFiles = new String[] {
+		"pageturn.ogg"
+	};
+
+	@SideOnly(Side.CLIENT)
+	@ForgeSubscribe
+	public void loadingSounds(SoundLoadEvent event) {
+		for (String soundFile : soundFiles) {
+			event.manager.addSound("openmodslib:" + soundFile);
+		}
+	}
+}
