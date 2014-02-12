066e10b67832c6a611143d608140797652b65d29
Mikee Franklin <mikeefranklin@gmail.com>
2014-02-12 18:10:55 +0000

    Fixed users getting a book every time

--- src/openmods/utils/PlayerUtils.java
+++ src/openmods/utils/PlayerUtils.java
@@ -25,16 +25,19 @@ public class PlayerUtils {
 	public static NBTTagCompound getModPlayerPersistTag(EntityPlayer player, String modName) {
 
 		NBTTagCompound tag = player.getEntityData();
-		NBTTagCompound persistTag = tag.getCompoundTag(EntityPlayer.PERSISTED_NBT_TAG);
-
-		if (persistTag == null) {
+		
+		NBTTagCompound persistTag = null;
+		if (tag.hasKey(EntityPlayer.PERSISTED_NBT_TAG)) {
+			persistTag = tag.getCompoundTag(EntityPlayer.PERSISTED_NBT_TAG);
+		} else {
 			persistTag = new NBTTagCompound();
 			tag.setCompoundTag(EntityPlayer.PERSISTED_NBT_TAG, persistTag);
 		}
-
-		NBTTagCompound modTag = persistTag.getCompoundTag(modName);
-
-		if (modTag == null) {
+		
+		NBTTagCompound modTag = null;
+		if (persistTag.hasKey(modName)) {
+			modTag = persistTag.getCompoundTag(modName);
+		} else {
 			modTag = new NBTTagCompound();
 			persistTag.setCompoundTag(modName, modTag);
 		}
