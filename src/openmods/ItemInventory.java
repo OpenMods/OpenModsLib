4123cf4849d3c3e6e126dada035da1feb0180aad
Bartek Bok <bartek.bok@gmail.com>
2014-03-25 18:12:39 +0100

    Unused import

--- src/openmods/ItemInventory.java
+++ src/openmods/ItemInventory.java
@@ -1,30 +1,28 @@
 package openmods;
 
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import openmods.api.IInventoryCallback;
 import openmods.utils.ItemUtils;
 
 public class ItemInventory extends GenericInventory {
 
 	public static final String TAG_INVENTORY = "inventory";
-	
+
 	private final EntityPlayer player;
 	private final ItemStack inventoryStack;
 	private final int inventorySlot;
-	
+
 	public ItemInventory(EntityPlayer _player, int size) {
 		super("", false, size);
-		
+
 		player = _player;
 		inventorySlot = player.inventory.currentItem;
 		inventoryStack = player.inventory.getCurrentItem();
 		final NBTTagCompound tag = ItemUtils.getItemTag(inventoryStack);
 		readFromNBT(getInventoryTag(tag));
 	}
-	
+
 	@Override
 	public void onInventoryChanged(int slotNumber) {
 		super.onInventoryChanged(slotNumber);
@@ -40,11 +38,9 @@ public class ItemInventory extends GenericInventory {
 		currentStack.setTagCompound(tag);
 		player.inventory.setInventorySlotContents(inventorySlot, currentStack);
 	}
-	
-	
+
 	public static NBTTagCompound getInventoryTag(NBTTagCompound tag) {
 		return tag.getCompoundTag(TAG_INVENTORY);
 	}
-
 
 }
