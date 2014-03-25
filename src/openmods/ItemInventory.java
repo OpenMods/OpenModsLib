ff004ae33235126b353e781df8a226924c09cdd4
Mikee Franklin <mikeefranklin@gmail.com>
2014-03-25 16:35:07 +0000

    Added item inventory to keep that kinda thing together.

---
+++ src/openmods/ItemInventory.java
@@ -0,0 +1,50 @@
+package openmods;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import openmods.api.IInventoryCallback;
+import openmods.utils.ItemUtils;
+
+public class ItemInventory extends GenericInventory {
+
+	public static final String TAG_INVENTORY = "inventory";
+	
+	private final EntityPlayer player;
+	private final ItemStack inventoryStack;
+	private final int inventorySlot;
+	
+	public ItemInventory(EntityPlayer _player, int size) {
+		super("", false, size);
+		
+		player = _player;
+		inventorySlot = player.inventory.currentItem;
+		inventoryStack = player.inventory.getCurrentItem();
+		final NBTTagCompound tag = ItemUtils.getItemTag(inventoryStack);
+		readFromNBT(getInventoryTag(tag));
+	}
+	
+	@Override
+	public void onInventoryChanged(int slotNumber) {
+		super.onInventoryChanged(slotNumber);
+		ItemStack currentStack = player.inventory.getCurrentItem();
+		if (currentStack == null || !currentStack.isItemEqual(inventoryStack)) {
+			player.closeScreen();
+			return;
+		}
+		NBTTagCompound tag = ItemUtils.getItemTag(currentStack);
+		NBTTagCompound inventoryTag = getInventoryTag(tag);
+		writeToNBT(inventoryTag);
+		tag.setTag(TAG_INVENTORY, inventoryTag);
+		currentStack.setTagCompound(tag);
+		player.inventory.setInventorySlotContents(inventorySlot, currentStack);
+	}
+	
+	
+	public static NBTTagCompound getInventoryTag(NBTTagCompound tag) {
+		return tag.getCompoundTag(TAG_INVENTORY);
+	}
+
+
+}
