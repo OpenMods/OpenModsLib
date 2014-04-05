95005fc1e82d7540afd5eabfdbe892e2932cc4c3
Bartek Bok <bartek.bok@gmail.com>
2014-04-05 14:47:04 +0200

    Add coremod loading order annotation

--- src/openmods/OpenModsCorePlugin.java
+++ src/openmods/OpenModsCorePlugin.java
@@ -5,8 +5,11 @@ import java.util.logging.Logger;
 
 import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin.SortingIndex;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;
 
+//must be lower than all dependent ones
+@SortingIndex(16)
 @TransformerExclusions({ "openmods.asm", "openmods.include" })
 public class OpenModsCorePlugin implements IFMLLoadingPlugin {
 
