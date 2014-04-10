11b50b5acb6683c8622f0805c9be54711a0fe8f7
Mikee Franklin <mikeefranklin@gmail.com>
2014-04-10 13:10:11 +0100

    Very basic first pass proof of concept for whitelist natural spawns

--- src/openmods/LibConfig.java
+++ src/openmods/LibConfig.java
@@ -13,4 +13,9 @@ public class LibConfig {
 	@ConfigProperty(category = "debug", name = "fakePlayerCountThreshold", comment = "Maximum fake player pool that can doesn't produce warning")
 	public static int fakePlayerThreshold = 10;
 
+	@ConfigProperty(category = "control", name = "enableNaturalSpawnWhitelist", comment = "Should the natural spawn whitelist be enabled?")
+	public static boolean enableNaturalSpawnWhitelist = false;
+	
+	@ConfigProperty(category = "control", name = "naturalSpawnWhitelist", comment = "List any mob names you will allow to spawn naturally")
+	public static String[] naturalSpawnWhitelist = new String[0];
 }
--- src/openmods/OpenMods.java
+++ src/openmods/OpenMods.java
@@ -1,7 +1,14 @@
 package openmods;
 
 import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.biome.SpawnListEntry;
 import net.minecraftforge.common.Configuration;
 import net.minecraftforge.common.MinecraftForge;
 import openmods.config.CommandConfig;
@@ -15,6 +22,9 @@ import openmods.network.PacketHandler;
 import openmods.network.events.TileEntityEventHandler;
 import openmods.proxy.IOpenModsProxy;
 import openmods.sync.SyncableManager;
+
+import com.google.common.collect.Sets;
+
 import cpw.mods.fml.common.*;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -41,7 +51,7 @@ public class OpenMods {
 		Configuration config = new Configuration(configFile);
 		ConfigProcessing.processAnnotations(configFile, "OpenMods", config, LibConfig.class);
 		if (config.hasChanged()) config.save();
-
+		
 		MinecraftForge.EVENT_BUS.register(new TileEntityEventHandler());
 
 		MinecraftForge.EVENT_BUS.register(DelayedEntityLoadManager.instance);
@@ -62,10 +72,50 @@ public class OpenMods {
 		Integration.addModule(new BuildCraftPipes());
 		Integration.loadModules();
 		proxy.postInit();
+		
+		if (LibConfig.enableNaturalSpawnWhitelist) removeNaturalMobSpawns();
 	}
 
 	@EventHandler
 	public void severStart(FMLServerStartingEvent evt) {
 		evt.registerServerCommand(new CommandConfig("om_config_s", true));
+	}
+
+	private void removeNaturalMobSpawns() {
+
+		Set<Class<?>> entityWhitelist = Sets.newIdentityHashSet();
+
+		Set<String> unknownNames = Sets.newHashSet();
+		for (String name : LibConfig.naturalSpawnWhitelist) {
+
+			Class<?> cls = (Class<?>)EntityList.stringToClassMapping.get(name);
+			if (cls != null) entityWhitelist.add(cls);
+			else unknownNames.add(name);
+		}
+
+		for (Class<?> cls : (Set<Class<?>>)EntityList.classToStringMapping.keySet()) {
+			if (unknownNames.isEmpty()) break;
+			if (unknownNames.remove(cls.getName())) entityWhitelist.add(cls);
+		}
+
+		if (!unknownNames.isEmpty()) Log.warn("Can't identify mobs for whitelist: %s", unknownNames);
+
+		for (BiomeGenBase biome : BiomeGenBase.biomeList) {
+			if (biome == null) continue;
+
+			for (EnumCreatureType type : EnumCreatureType.values()) {
+				List<SpawnListEntry> spawnableEntries = (List<SpawnListEntry>)biome.getSpawnableList(type);
+				if (spawnableEntries != null) {
+					Iterator<SpawnListEntry> it = spawnableEntries.iterator();
+					while (it.hasNext()) {
+						SpawnListEntry entry = it.next();
+						if (!entityWhitelist.contains(entry.entityClass)) {
+							System.out.println("Removing " + entry.entityClass);
+							it.remove();
+						}
+					}
+				}
+			}
+		}
 	}
 }
