aeb5088d9046b568cbef964f000cdc5bbd85ed7f
Bartek Bok <bartek.bok@gmail.com>
2014-05-18 00:30:22 +0200

    Some kind of unit thingie

---
+++ src/main/java/openmods/utils/Units.java
@@ -0,0 +1,44 @@
+package openmods.utils;
+
+public class Units {
+	public static enum SpeedUnit {
+		M_PER_TICK("m/tick", 1, "%.2f %s"),
+		M_PER_S("m/s", 1.0 * 20.0, "%.2f %s"), // probably not working
+		KM_PER_H("km/s", 3.6 * 20.0, "%.2f %s"),
+		BROKEN("mph", 2.23694 * 20.0, "%.2f %s");
+
+		public final String name;
+		public final double factor;
+		public final String format;
+
+		private SpeedUnit(String name, double factor, String format) {
+			this.name = name;
+			this.factor = factor;
+			this.format = format;
+		}
+
+		public String format(double value) {
+			return String.format(format, value * factor, name);
+		}
+	}
+
+	public static enum DistanceUnit {
+		M("m", 1, "%.0f %s"),
+		KM("km", 0.001, "%.2f %s"),
+		SILLY("miles", 0.000621371, "%.f %s");
+
+		public final String name;
+		public final double factor;
+		public final String format;
+
+		private DistanceUnit(String name, double factor, String format) {
+			this.name = name;
+			this.factor = factor;
+			this.format = format;
+		}
+
+		public String format(double value) {
+			return String.format(format, value * factor, name);
+		}
+	}
+}
