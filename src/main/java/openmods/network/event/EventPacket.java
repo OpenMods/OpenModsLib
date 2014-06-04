e3aac4c20598b6a1b96a540277f6beb8502a828c
Bartek Bok <bartek.bok@gmail.com>
2014-06-04 18:36:53 +0200

    Remove junk

--- src/main/java/openmods/network/event/EventPacket.java
+++ src/main/java/openmods/network/event/EventPacket.java
@@ -3,10 +3,7 @@ package openmods.network.event;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-import java.util.Collection;
 import java.util.List;
-
-import net.minecraft.entity.player.EntityPlayer;
 
 import com.google.common.collect.Lists;
 
@@ -25,43 +22,8 @@ public abstract class EventPacket extends Event {
 
 	protected void appendLogInfo(List<String> info) {}
 
-	protected boolean checkSendToClient() {
-		// if (!getType().getDirection().toClient) {
-		// Log.warn("Trying to sent message '%s' to client", this);
-		// return false;
-		// }
-		return true;
-	}
-
-	protected boolean checkSendToServer() {
-		// if (!getType().getDirection().toServer) {
-		// Log.warn("Trying to sent message '%s' to server", this);
-		// return false;
-		// }
-		return true;
-	}
-
 	public void reply(EventPacket reply) {
 		reply.dispatcher = dispatcher;
 		this.replies.add(reply);
-	}
-
-	public void sendToPlayer(EntityPlayer player) {
-		// sendToPlayer((Player)player);
-	}
-
-	public void sendToPlayers(Collection<EntityPlayer> players) {
-		// if (checkSendToClient()) {
-		// for (Packet packet : EventPacketManager.serializeEvent(this))
-		// for (EntityPlayer player : players)
-		// OpenMods.proxy.sendPacketToPlayer((Player)player, packet);
-		// }
-	}
-
-	public void sendToServer() {
-		// if (checkSendToServer()) {
-		// for (Packet packet : EventPacketManager.serializeEvent(this))
-		// OpenMods.proxy.sendPacketToServer(packet);
-		// }
 	}
 }
