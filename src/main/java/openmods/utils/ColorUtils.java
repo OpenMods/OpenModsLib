91ab8a29499bae7a3ef9ee3a83ce7c2c0a0a8c57
Bartek Bok <bartek.bok@gmail.com>
2014-11-03 20:58:08 +0100

    Make colors actual enums

--- src/main/java/openmods/utils/ColorUtils.java
+++ src/main/java/openmods/utils/ColorUtils.java
@@ -1,57 +1,40 @@
 package openmods.utils;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockColored;
-import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 
 import org.apache.commons.lang3.text.WordUtils;
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.Lists;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 
 public class ColorUtils {
+	public static enum ColorMeta {
+		BLACK("black", 0x1E1B1B),
+		RED("red", 0xB3312C),
+		GREEN("green", 0x3B511A),
+		BROWN("brown", 0x51301A),
+		BLUE("blue", 0x253192),
+		PURPLE("purple", 0x7B2FBE),
+		CYAN("cyan", 0x287697),
+		LIGHT_GRAY("lightGray", 0xABABAB),
+		GRAY("gray", 0x434343),
+		PINK("pink", 0xD88198),
+		LIME("lime", 0x41CD34),
+		YELLOW("yellow", 0xDECF2A),
+		LIGHT_BLUE("lightBlue", 0x6689D3),
+		MAGENTA("magenta", 0xC354CD),
+		ORANGE("orange", 0xEB8844),
+		WHITE("white", 0xF0F0F0);
 
-	public static final int BLACK = 0;
-	public static final int RED = 1;
-	public static final int GREEN = 2;
-	public static final int BROWN = 3;
-	public static final int BLUE = 4;
-	public static final int PURPLE = 5;
-	public static final int CYAN = 6;
-	public static final int LIGHT_GRAY = 7;
-	public static final int GRAY = 8;
-	public static final int PINK = 9;
-	public static final int LIME = 10;
-	public static final int YELLOW = 11;
-	public static final int LIGHT_BLUE = 12;
-	public static final int MAGENTA = 13;
-	public static final int ORANGE = 14;
-	public static final int WHITE = 15;
-
-	public static ItemStack createDyedWool(int color) {
-		return createDyedBlock(Blocks.wool, color);
-	}
-
-	public static ItemStack createStainedClay(int color) {
-		return createDyedBlock(Blocks.stained_hardened_clay, color);
-	}
-
-	public static ItemStack createDyedCarpet(int color) {
-		return createDyedBlock(Blocks.carpet, color);
-	}
-
-	public static ItemStack createDyedBlock(Block block, int color) {
-		int blockColor = BlockColored.func_150031_c(color);
-		return new ItemStack(block, 1, blockColor);
-	}
-
-	public static class ColorMeta {
 		public final int rgb;
 		public final int vanillaDyeId;
 		public final int vanillaBlockId;
@@ -60,52 +43,41 @@ public class ColorUtils {
 		public final String oreName;
 		public final String name;
 
-		private ColorMeta(int rgb, int index, int oreId, String name, String oreName) {
+		public ItemStack createStack(Block block, int amount) {
+			return new ItemStack(block, amount, vanillaBlockId);
+		}
+
+		public ItemStack createStack(Item item, int amount) {
+			return new ItemStack(item, amount, vanillaDyeId);
+		}
+
+		private ColorMeta(String name, int rgb) {
+			this.oreName = "dye" + WordUtils.capitalize(name);
+			this.oreId = OreDictionary.getOreID(oreName);
+			this.name = name.toLowerCase();
 			this.rgb = rgb;
+
+			final int index = ordinal();
 			this.vanillaDyeId = index;
 			this.vanillaBlockId = ~index & 15;
-			this.oreId = oreId;
-			this.oreName = oreName;
-			this.name = name;
 			this.bitmask = 1 << index;
 		}
+
+		private static final ColorMeta[] COLORS = ColorMeta.values();
 	}
 
-	private static final List<ColorMeta> COLORS = Lists.newArrayList();
 	private static final Map<String, ColorMeta> COLORS_BY_ORE_NAME = Maps.newHashMap();
 	private static final Map<String, ColorMeta> COLORS_BY_NAME = Maps.newHashMap();
 	private static final Map<Integer, ColorMeta> COLORS_BY_ORE_ID = Maps.newHashMap();
 	private static final Map<Integer, ColorMeta> COLORS_BY_BITMASK = Maps.newHashMap();
 
-	private static void addEntry(String name, int colorValue, int index) {
-		String oreName = "dye" + WordUtils.capitalize(name);
-		int oreId = OreDictionary.getOreID(oreName);
-		name = name.toLowerCase();
-		ColorMeta color = new ColorMeta(colorValue, index, oreId, name, oreName);
-		COLORS.add(color);
-		COLORS_BY_NAME.put(name, color);
-		COLORS_BY_ORE_NAME.put(oreName, color);
-		COLORS_BY_ORE_ID.put(oreId, color);
-		COLORS_BY_BITMASK.put(color.bitmask, color);
-	}
-
 	static {
-		addEntry("black", 0x1E1B1B, BLACK);
-		addEntry("red", 0xB3312C, RED);
-		addEntry("green", 0x3B511A, GREEN);
-		addEntry("brown", 0x51301A, BROWN);
-		addEntry("blue", 0x253192, BLUE);
-		addEntry("purple", 0x7B2FBE, PURPLE);
-		addEntry("cyan", 0x287697, CYAN);
-		addEntry("lightGray", 0xABABAB, LIGHT_GRAY);
-		addEntry("gray", 0x434343, GRAY);
-		addEntry("pink", 0xD88198, PINK);
-		addEntry("lime", 0x41CD34, LIME);
-		addEntry("yellow", 0xDECF2A, YELLOW);
-		addEntry("lightBlue", 0x6689D3, LIGHT_BLUE);
-		addEntry("magenta", 0xC354CD, MAGENTA);
-		addEntry("orange", 0xEB8844, ORANGE);
-		addEntry("white", 0xF0F0F0, WHITE);
+		for (ColorMeta color : ColorMeta.COLORS) {
+			COLORS_BY_NAME.put(color.name, color);
+			COLORS_BY_ORE_NAME.put(color.oreName, color);
+			COLORS_BY_ORE_ID.put(color.oreId, color);
+			COLORS_BY_BITMASK.put(color.bitmask, color);
+		}
 	}
 
 	public static Set<ColorMeta> stackToColor(ItemStack stack) {
@@ -134,15 +106,15 @@ public class ColorUtils {
 	}
 
 	public static ColorMeta vanillaBlockToColor(int vanillaId) {
-		return COLORS.get(~vanillaId & 0xF);
+		return ColorMeta.COLORS[~vanillaId & 0xF];
 	}
 
 	public static ColorMeta vanillaDyeToColor(int vanillaId) {
-		return COLORS.get(vanillaId & 0xF);
+		return ColorMeta.COLORS[vanillaId & 0xF];
 	}
 
 	public static Collection<ColorMeta> getAllColors() {
-		return Collections.unmodifiableCollection(COLORS);
+		return ImmutableList.copyOf(ColorMeta.COLORS);
 	}
 
 	public static int bitmaskToVanilla(int color) {
