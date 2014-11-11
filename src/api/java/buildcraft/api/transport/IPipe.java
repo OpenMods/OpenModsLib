183ce890e49fd4387594316d6dbfc8953abbff69
Bartek Bok <bartek.bok@gmail.com>
2014-11-11 13:02:12 +0100

    Update BC API

--- src/api/java/buildcraft/api/transport/IPipe.java
+++ src/api/java/buildcraft/api/transport/IPipe.java
@@ -6,10 +6,26 @@
  * License 1.0, or MMPL. Please check the contents of the license located in
  * http://www.mod-buildcraft.com/MMPL-1.0.txt
  */
-package buildcraft.api.mj;
+package buildcraft.api.transport;
 
 import net.minecraftforge.common.util.ForgeDirection;
+import buildcraft.api.gates.IGate;
 
-public interface ISidedBatteryProvider {
-	IBatteryObject getMjBattery(String kind, ForgeDirection direction);
+public interface IPipe {
+
+	int x();
+
+	int y();
+
+	int z();
+
+	IPipeTile getTile();
+
+	IGate getGate(ForgeDirection side);
+	
+	boolean hasGate(ForgeDirection side);
+	
+	boolean isWired(PipeWire wire);
+	
+	boolean isWireActive(PipeWire wire);
 }
