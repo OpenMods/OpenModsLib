013c6071ba0e84151457895d1b8e24fe8d6fad8a
Bartek Bok <bartek.bok@gmail.com>
2014-11-22 12:31:51 +0100

    Few texture utils

--- src/main/java/openmods/utils/TextureUtils.java
+++ src/main/java/openmods/utils/TextureUtils.java
@@ -9,6 +9,28 @@ import openmods.Log;
 
 public final class TextureUtils {
 
+	public final static int TEXTURE_MAP_BLOCKS = 0;
+	public final static int TEXTURE_MAP_ITEMS = 1;
+
+	public static void bindItemStackTexture(ItemStack itemStack) {
+		bindIconSheet(itemStack.getItemSpriteNumber());
+	}
+
+	public static void bindIconSheet(int type) {
+		final Minecraft mc = Minecraft.getMinecraft();
+		if (mc != null) {
+			TextureManager manager = mc.getTextureManager();
+			bindIconSheet(manager, type);
+		} else {
+			Log.warn("Binding texture to null client.");
+		}
+	}
+
+	public static void bindIconSheet(TextureManager manager, int type) {
+		final ResourceLocation resourceLocation = manager.getResourceLocation(type);
+		manager.bindTexture(resourceLocation);
+	}
+
 	public static void bindTextureToClient(ResourceLocation texture) {
 		if (texture != null) {
 			final Minecraft mc = Minecraft.getMinecraft();
@@ -32,16 +54,5 @@ public final class TextureUtils {
 
 	public static int getRandomNumber() {
 		return 4;
-	}
-
-	public static void bindItemStackTexture(ItemStack itemStack) {
-		final Minecraft mc = Minecraft.getMinecraft();
-		if (mc != null) {
-			TextureManager manager = mc.getTextureManager();
-			final ResourceLocation resourceLocation = manager.getResourceLocation(itemStack.getItemSpriteNumber());
-			manager.bindTexture(resourceLocation);
-		} else {
-			Log.warn("Binding texture to null client.");
-		}
 	}
 }
