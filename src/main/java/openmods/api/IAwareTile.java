1b28940c6956ecf1ea20a1d14654418ea9a34286
Bartek Bok <bartek.bok@gmail.com>
2014-12-23 19:17:47 +0100

    Remove deprecated interface

--- src/main/java/openmods/api/IAwareTile.java
+++
@@ -1,7 +0,0 @@
-package openmods.api;
-
-/**
- * @deprecated Implement interfaces that you actually need
- */
-@Deprecated
-public interface IAwareTile extends INeighbourAwareTile, IBreakAwareTile, IPlaceAwareTile, IActivateAwareTile {}
