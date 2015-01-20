cfe6735ebd0d2f76cff63abfb4adf31858e384f3
Bartek Bok <bartek.bok@gmail.com>
2015-01-20 22:55:36 +0100

    Add modid to list

--- src/main/java/openmods/Mods.java
+++ src/main/java/openmods/Mods.java
@@ -37,6 +37,7 @@ public class Mods {
 	public static final String MULTIPART = "McMultipart";
 	public static final String MYSTCRAFT = "Mystcraft";
 	public static final String OPENBLOCKS = "OpenBlocks";
+	public static final String OPENCOMPUTERS = "OpenComputers";
 	public static final String OPENPERIPHERAL = "OpenPeripheral";
 	public static final String OPENPERIPHERALCORE = "OpenPeripheralCore";
 	public static final String PORTALGUN = "PortalGun";
