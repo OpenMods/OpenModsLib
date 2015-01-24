67df2d608305cdaeebd7c3dea08047aa37ddb57f
Bartek Bok <bartek.bok@gmail.com>
2015-01-24 19:24:16 +0100

    Add NBT hasher

--- src/main/java/openmods/utils/ItemUtils.java
+++ src/main/java/openmods/utils/ItemUtils.java
@@ -1,13 +1,22 @@
 package openmods.utils;
 
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+import java.security.DigestOutputStream;
+import java.security.MessageDigest;
+
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTBase;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagInt;
+import net.minecraft.nbt.*;
 import net.minecraft.world.World;
+
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.io.output.NullOutputStream;
+
+import com.google.common.base.Throwables;
 
 public class ItemUtils {
 
@@ -39,5 +48,22 @@ public class ItemUtils {
 
 	public static EntityItem createEntityItem(World world, double x, double y, double z, ItemStack is) {
 		return new EntityItem(world, x, y, z, is.copy());
+	}
+
+	/**
+	 * This function returns fingerprint of NBTTag. It can be used to compare two tags
+	 */
+	public static String getNBTHash(NBTTagCompound tag) {
+		try {
+			MessageDigest digest = MessageDigest.getInstance("MD5");
+			OutputStream dump = new NullOutputStream();
+			DigestOutputStream hasher = new DigestOutputStream(dump, digest);
+			DataOutput output = new DataOutputStream(hasher);
+			CompressedStreamTools.write(tag, output);
+			byte[] hash = digest.digest();
+			return new String(Hex.encodeHex(hash));
+		} catch (Exception e) {
+			throw Throwables.propagate(e);
+		}
 	}
 }
