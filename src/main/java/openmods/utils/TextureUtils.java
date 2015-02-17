200ab2f597befcf616c657b6b3bead13ffc33b65
William Jackson <Strikingwolf2012@gmail.com>
2015-02-16 20:37:50 -0600

    Update TextureUtils.java

--- src/main/java/openmods/utils/TextureUtils.java
+++ src/main/java/openmods/utils/TextureUtils.java
@@ -53,7 +53,7 @@ public final class TextureUtils {
 	}
 
 	public static int getRandomNumber() {
-		return 4; // chosen by fair dice roll
-		          // guranteed to be random
+		return 4; // chosen by fair dice roll.
+		          // guranteed to be random.
 	}
 }
