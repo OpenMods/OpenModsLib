0eeb92acf8ddc2be64b27bfbf60422046acab7ad
Bartek Bok <bartek.bok@gmail.com>
2015-06-09 21:27:49 +0200

    Make SyncableUUID nullable

--- src/main/java/openmods/sync/SyncableUUID.java
+++ src/main/java/openmods/sync/SyncableUUID.java
@@ -6,45 +6,71 @@ import java.io.IOException;
 import java.util.UUID;
 
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.common.util.Constants;
+import openmods.api.IValueProvider;
 
-public class SyncableUUID extends SyncableObjectBase {
+import com.google.common.base.Objects;
+
+public class SyncableUUID extends SyncableObjectBase implements IValueProvider<UUID> {
 
 	private UUID uuid;
 
 	@Override
 	public void readFromStream(DataInputStream stream) throws IOException {
-		long msb = stream.readLong();
-		long lsb = stream.readLong();
-		uuid = new UUID(msb, lsb);
+		if (stream.readBoolean()) {
+			long msb = stream.readLong();
+			long lsb = stream.readLong();
+			this.uuid = new UUID(msb, lsb);
+		} else {
+			this.uuid = null;
+		}
 	}
 
 	@Override
 	public void writeToStream(DataOutputStream stream) throws IOException {
-		stream.writeLong(uuid.getMostSignificantBits());
-		stream.writeLong(uuid.getLeastSignificantBits());
+		if (uuid != null) {
+			stream.writeBoolean(true);
+			stream.writeLong(uuid.getMostSignificantBits());
+			stream.writeLong(uuid.getLeastSignificantBits());
+		} else {
+			stream.writeBoolean(false);
+		}
 	}
 
 	@Override
 	public void writeToNBT(NBTTagCompound nbt, String name) {
-		NBTTagCompound result = new NBTTagCompound();
-		result.setLong("MSB", uuid.getMostSignificantBits());
-		result.setLong("LSB", uuid.getLeastSignificantBits());
-		nbt.setTag(name, result);
+		if (uuid != null) {
+			NBTTagCompound result = new NBTTagCompound();
+			result.setLong("MSB", uuid.getMostSignificantBits());
+			result.setLong("LSB", uuid.getLeastSignificantBits());
+			nbt.setTag(name, result);
+		}
 	}
 
 	@Override
 	public void readFromNBT(NBTTagCompound nbt, String name) {
-		NBTTagCompound data = nbt.getCompoundTag(name);
-		long msb = data.getLong("MSB");
-		long lsb = data.getLong("LSB");
-		uuid = new UUID(msb, lsb);
+		if (nbt.hasKey(name, Constants.NBT.TAG_COMPOUND)) {
+			NBTTagCompound data = nbt.getCompoundTag(name);
+			long msb = data.getLong("MSB");
+			long lsb = data.getLong("LSB");
+			this.uuid = new UUID(msb, lsb);
+		} else {
+			this.uuid = null;
+		}
 	}
 
 	public void setValue(UUID value) {
-		this.uuid = value;
-		markDirty();
+		if (!Objects.equal(uuid, value)) {
+			this.uuid = value;
+			markDirty();
+		}
 	}
 
+	public void clear() {
+		setValue(null);
+	}
+
+	@Override
 	public UUID getValue() {
 		return uuid;
 	}
