eebeb4c681fd63cf2eef4b7734f8e7adc6173e3c
Bartek Bok <bartek.bok@gmail.com>
2015-09-16 22:01:03 +0200

    New display list utils

--- src/main/java/openmods/renderer/DisplayListWrapper.java
+++ src/main/java/openmods/renderer/DisplayListWrapper.java
@@ -4,23 +4,25 @@ import org.lwjgl.opengl.GL11;
 
 public abstract class DisplayListWrapper {
 
-	private Integer displayList;
+	private int displayList;
 
-	@Override
-	protected void finalize() {
-		reset();
-	}
+	private boolean isValid;
+
+	private boolean pendingInvalidate;
 
 	public boolean isCompiled() {
-		return displayList != null;
+		return isValid;
 	}
 
 	public void render() {
-		if (displayList == null) {
+		if (pendingInvalidate) reset();
+
+		if (!isValid) {
 			displayList = GL11.glGenLists(1);
 			GL11.glNewList(displayList, GL11.GL_COMPILE);
 			compile();
 			GL11.glEndList();
+			isValid = true;
 		}
 
 		GL11.glCallList(displayList);
@@ -28,10 +30,15 @@ public abstract class DisplayListWrapper {
 
 	public abstract void compile();
 
+	/**
+	 * WARNING: this method can be only used in client (rendering) thread. If not possible, use {@link #invalidate()}.
+	 */
 	public void reset() {
-		if (displayList != null) {
-			GL11.glDeleteLists(displayList, 1);
-			displayList = null;
-		}
+		if (isValid) GL11.glDeleteLists(displayList, 1);
+		pendingInvalidate = isValid = false;
+	}
+
+	public void invalidate() {
+		pendingInvalidate = true;
 	}
 }
---
+++ src/main/java/openmods/renderer/ManualDisplayList.java
@@ -0,0 +1,43 @@
+package openmods.renderer;
+
+import org.lwjgl.opengl.GL11;
+
+import com.google.common.base.Preconditions;
+
+public class ManualDisplayList {
+
+	public interface Renderer {
+		public void render();
+	}
+
+	private int displayList;
+
+	private boolean isAllocated;
+
+	private boolean isValid;
+
+	public boolean isCompiled() {
+		return isValid;
+	}
+
+	public void render() {
+		Preconditions.checkState(isValid, "Display list not initialized");
+		GL11.glCallList(displayList);
+	}
+
+	public void compile(Renderer renderer) {
+		if (isAllocated) GL11.glDeleteLists(displayList, 1);
+
+		displayList = GL11.glGenLists(1);
+		GL11.glNewList(displayList, GL11.GL_COMPILE);
+		renderer.render();
+		GL11.glEndList();
+
+		isAllocated = isValid = true;
+	}
+
+	public void invalidate() {
+		isValid = false;
+	}
+
+}
