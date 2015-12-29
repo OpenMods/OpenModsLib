113d6e24efd17acd2040f5545003e3cbb9b62ff2
Bartek Bok <bartek.bok@gmail.com>
2015-12-29 19:22:11 +0100

    Temporarily disable Buildcraft integration

--- src/main/java/openmods/integration/modules/BuildCraftPipes.java
+++ src/main/java/openmods/integration/modules/BuildCraftPipes.java
@@ -3,12 +3,11 @@ package openmods.integration.modules;
 import static openmods.integration.Conditions.classExists;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraftforge.common.util.ForgeDirection;
+import net.minecraft.util.EnumFacing;
 import openmods.integration.IntegrationModule;
 import openmods.inventory.legacy.CustomSinks;
 import openmods.inventory.legacy.CustomSinks.ICustomSink;
 import openmods.inventory.legacy.CustomSinks.ICustomSinkProvider;
-import buildcraft.api.transport.IPipeTile;
 
 public class BuildCraftPipes extends IntegrationModule {
 
@@ -17,14 +16,14 @@ public class BuildCraftPipes extends IntegrationModule {
 	}
 
 	public interface BuildCraftAccess {
-		public int tryAcceptIntoPipe(TileEntity possiblePipe, ItemStack nextStack, boolean doInsert, ForgeDirection direction);
+		public int tryAcceptIntoPipe(TileEntity possiblePipe, ItemStack nextStack, boolean doInsert, EnumFacing direction);
 
 		public boolean isPipe(TileEntity tile);
 	}
 
 	private static class Dummy implements BuildCraftAccess {
 		@Override
-		public int tryAcceptIntoPipe(TileEntity possiblePipe, ItemStack nextStack, boolean doInsert, ForgeDirection direction) {
+		public int tryAcceptIntoPipe(TileEntity possiblePipe, ItemStack nextStack, boolean doInsert, EnumFacing direction) {
 			return 0;
 		}
 
@@ -37,14 +36,14 @@ public class BuildCraftPipes extends IntegrationModule {
 	private static class Live implements BuildCraftAccess {
 
 		@Override
-		public int tryAcceptIntoPipe(TileEntity possiblePipe, ItemStack nextStack, boolean doInsert, ForgeDirection direction) {
-			if (possiblePipe instanceof IPipeTile) return ((IPipeTile)possiblePipe).injectItem(nextStack, doInsert, direction.getOpposite());
+		public int tryAcceptIntoPipe(TileEntity possiblePipe, ItemStack nextStack, boolean doInsert, EnumFacing direction) {
+			// if (possiblePipe instanceof IPipeTile) return ((IPipeTile)possiblePipe).injectItem(nextStack, doInsert, direction.getOpposite());
 			return 0;
 		}
 
 		@Override
 		public boolean isPipe(TileEntity tile) {
-			return tile instanceof IPipeTile;
+			return false; // tile instanceof IPipeTile;
 		}
 	}
 
@@ -59,14 +58,13 @@ public class BuildCraftPipes extends IntegrationModule {
 		access = new Live();
 
 		CustomSinks.registerCustomSink(new ICustomSinkProvider() {
-
 			@Override
 			public CustomSinks.ICustomSink create(final TileEntity te) {
 				if (!access.isPipe(te)) return null;
 
 				return new ICustomSink() {
 					@Override
-					public int accept(ItemStack stack, boolean doInsert, ForgeDirection direction) {
+					public int accept(ItemStack stack, boolean doInsert, EnumFacing direction) {
 						return access.tryAcceptIntoPipe(te, stack, doInsert, direction);
 					}
 				};
