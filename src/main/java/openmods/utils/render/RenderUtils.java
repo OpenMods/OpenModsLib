c67fc0257aab7788a29123b39275ddfed311bd3c
Bartek Bok <bartek.bok@gmail.com>
2017-02-26 17:27:33 +0100

    Small optimization

--- src/main/java/openmods/utils/render/RenderUtils.java
+++ src/main/java/openmods/utils/render/RenderUtils.java
@@ -118,6 +118,7 @@ public class RenderUtils {
 	}
 
 	public static void renderInventoryBlockNoBounds(RenderBlocks renderer, Block block, int metadata, int colorMultiplier, Set<ForgeDirection> enabledSides) {
+		if (enabledSides.isEmpty()) return;
 		Tessellator tessellator = Tessellator.instance;
 		float r;
 		float g;
@@ -132,42 +133,34 @@ public class RenderUtils {
 		GL11.glPushMatrix();
 		GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
 		GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
+
+		tessellator.startDrawingQuads();
 		if (enabledSides.contains(ForgeDirection.DOWN)) {
-			tessellator.startDrawingQuads();
 			tessellator.setNormal(0.0F, -1.0F, 0.0F);
 			renderer.renderFaceYNeg(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 0, metadata));
-			tessellator.draw();
 		}
 		if (enabledSides.contains(ForgeDirection.UP)) {
-			tessellator.startDrawingQuads();
 			tessellator.setNormal(0.0F, 1.0F, 0.0F);
 			renderer.renderFaceYPos(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 1, metadata));
-			tessellator.draw();
 		}
 		if (enabledSides.contains(ForgeDirection.SOUTH)) {
-			tessellator.startDrawingQuads();
 			tessellator.setNormal(0.0F, 0.0F, -1.0F);
 			renderer.renderFaceZNeg(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 2, metadata));
-			tessellator.draw();
 		}
 		if (enabledSides.contains(ForgeDirection.NORTH)) {
-			tessellator.startDrawingQuads();
 			tessellator.setNormal(0.0F, 0.0F, 1.0F);
 			renderer.renderFaceZPos(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 3, metadata));
-			tessellator.draw();
 		}
 		if (enabledSides.contains(ForgeDirection.WEST)) {
-			tessellator.startDrawingQuads();
 			tessellator.setNormal(-1.0F, 0.0F, 0.0F);
 			renderer.renderFaceXNeg(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 4, metadata));
-			tessellator.draw();
 		}
 		if (enabledSides.contains(ForgeDirection.EAST)) {
-			tessellator.startDrawingQuads();
 			tessellator.setNormal(1.0F, 0.0F, 0.0F);
 			renderer.renderFaceXPos(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 5, metadata));
-			tessellator.draw();
 		}
+
+		tessellator.draw();
 		GL11.glPopMatrix();
 	}
 
