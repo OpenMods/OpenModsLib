a4fc43d4d0f7295839e5d3b36e561a1e4c857436
Bartek Bok <bartek.bok@gmail.com>
2017-05-04 20:56:54 +0200

    Making it more robust (I hope...)

--- src/main/java/openmods/core/BundledJarUnpacker.java
+++ src/main/java/openmods/core/BundledJarUnpacker.java
@@ -1,45 +1,92 @@
 package openmods.core;
 
+import com.google.common.base.Optional;
 import com.google.common.base.Splitter;
-import com.google.common.io.Files;
+import com.google.common.io.ByteStreams;
+import com.google.common.io.Closer;
 import java.io.File;
-import java.io.InputStream;
+import java.io.IOException;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.util.Map;
 import java.util.jar.JarFile;
 import java.util.zip.ZipEntry;
+import jline.internal.Log;
 import net.minecraft.launchwrapper.LaunchClassLoader;
+import openmods.utils.io.Locks;
+import openmods.utils.io.Locks.ExclusiveLock;
+import openmods.utils.io.Locks.SharedLock;
 
 public class BundledJarUnpacker {
 
 	private static final String JAR_JARS_ATTRIBUTE = "JarJars";
 
+	private static ExclusiveLock findLockableFile(File dir, String name) throws IOException {
+		for (int i = 0; i < 1000; i++) {
+			final File subDir = new File(dir, Integer.toString(i));
+
+			if (subDir.isFile()) continue;
+
+			if (!subDir.exists())
+				if (!subDir.mkdirs()) continue;
+
+			final File file = new File(subDir, name);
+			final Optional<ExclusiveLock> lock = Locks.tryExclusiveLock(file);
+			if (lock.isPresent()) return lock.get();
+		}
+
+		throw new IllegalStateException("Failed to find temporary dir for libs in " + dir);
+	}
+
 	public static void setup(Map<String, Object> data) {
 		try {
 			final File coremodFile = (File)data.get("coremodLocation");
+			if (coremodFile == null) return;
+
 			final LaunchClassLoader classLoader = (LaunchClassLoader)data.get("classLoader");
 
+			final File tmpDir = new File(System.getProperty("java.io.tmpdir"));
+			final File libDir = new File(tmpDir, "mc_libs");
+			if (!libDir.exists()) libDir.mkdir();
+
 			final JarFile coremodJar = new JarFile(coremodFile);
+			final Closer closer = Closer.create();
 			try {
 				final String jarJars = coremodJar.getManifest().getMainAttributes().getValue(JAR_JARS_ATTRIBUTE);
-
-				final File jarJarsDir = Files.createTempDir();
-				jarJarsDir.deleteOnExit();
 
 				for (String jarJar : Splitter.on(" ").split(jarJars)) {
 					final ZipEntry entry = coremodJar.getEntry(jarJar);
 					if (entry == null) throw new IllegalAccessException("Can't find entry " + jarJar + " in jar " + coremodFile);
-					final File jarJarTmpFile = new File(jarJarsDir, jarJar);
-					jarJarTmpFile.deleteOnExit();
 
-					final InputStream jarJarStream = coremodJar.getInputStream(entry);
-					Files.asByteSink(jarJarTmpFile).writeFrom(jarJarStream);
+					final ExclusiveLock lockedFile = findLockableFile(libDir, jarJar);
+
+					Log.debug("Copying file %s from %s to %s", jarJar, coremodFile, lockedFile.file);
+					final ReadableByteChannel jarJarStream = Channels.newChannel(coremodJar.getInputStream(entry));
+					ByteStreams.copy(jarJarStream, lockedFile.channel);
 					jarJarStream.close();
 
-					classLoader.addURL(jarJarTmpFile.toURI().toURL());
+					// keep shared lock to prevent file overwrite
+					final SharedLock sharedFile = closer.register(lockedFile.degrade());
+					sharedFile.file.deleteOnExit();
+
+					classLoader.addURL(sharedFile.file.toURI().toURL());
 				}
+			} catch (Throwable t) {
+				throw closer.rethrow(t);
 			} finally {
 				coremodJar.close();
 			}
+
+			Runtime.getRuntime().addShutdownHook(new Thread() {
+				@Override
+				public void run() {
+					try {
+						closer.close();
+					} catch (IOException e) {
+						// welp
+					}
+				}
+			});
 		} catch (Exception e) {
 			throw new IllegalStateException("Failed to inject dependecies, data: " + data.toString(), e);
 		}
---
+++ src/main/java/openmods/utils/io/Locks.java
@@ -0,0 +1,156 @@
+package openmods.utils.io;
+
+import com.google.common.base.Optional;
+import com.google.common.io.Closer;
+import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+
+public class Locks {
+
+	public abstract static class Lock implements Closeable {
+		public final File file;
+
+		public final FileChannel channel;
+
+		protected final FileLock lock;
+
+		private Lock(File file, FileChannel channel, FileLock lock) {
+			this.file = file;
+			this.channel = channel;
+			this.lock = lock;
+		}
+
+		public boolean isValid() {
+			return lock.isValid();
+		}
+
+		public abstract boolean isShared();
+
+		@Override
+		public void close() throws IOException {
+			lock.release();
+			channel.close();
+		}
+
+		public abstract SharedLock degrade() throws IOException;
+
+		public abstract ExclusiveLock upgrade() throws IOException;
+	}
+
+	public static class ExclusiveLock extends Lock {
+		private ExclusiveLock(File file, FileChannel fc, FileLock lock) {
+			super(file, fc, lock);
+			if (lock.isShared()) throw new AssertionError("Invalid lock: " + lock);
+		}
+
+		@Override
+		public SharedLock degrade() throws IOException {
+			// Can't find way to do it as atomic operation.
+			// This may cause some glitches and creates attach area (gasp!)
+			lock.release();
+			final FileLock sharedLock = channel.lock(0, Long.MAX_VALUE, true);
+			if (sharedLock == null) throw new IllegalStateException("Failed to re-lock file " + file);
+			return new SharedLock(file, channel, sharedLock);
+		}
+
+		@Override
+		public ExclusiveLock upgrade() {
+			return this;
+		}
+
+		@Override
+		public boolean isShared() {
+			return false;
+		}
+	}
+
+	public static class SharedLock extends Lock {
+		private SharedLock(File file, FileChannel fc, FileLock lock) {
+			super(file, fc, lock);
+			if (!lock.isShared()) throw new AssertionError("Invalid lock: " + lock);
+		}
+
+		@Override
+		public SharedLock degrade() {
+			return this;
+		}
+
+		@Override
+		public ExclusiveLock upgrade() throws IOException {
+			// Can't find way to do it as atomic operation.
+			// This may cause some glitches and creates attach area (gasp!)
+			lock.release();
+			final FileLock exclusiveLock = channel.tryLock();
+			if (exclusiveLock == null) throw new IllegalStateException("Failed to re-lock file " + file);
+			return new ExclusiveLock(file, channel, exclusiveLock);
+		}
+
+		@Override
+		public boolean isShared() {
+			return true;
+		}
+	}
+
+	public static Optional<ExclusiveLock> tryExclusiveLock(File file) throws IOException {
+		Closer closer = Closer.create();
+		try {
+			final RandomAccessFile s = closer.register(new RandomAccessFile(file, "rw"));
+			final FileChannel ch = closer.register(s.getChannel());
+
+			final FileLock lock = ch.tryLock();
+			if (lock != null) return Optional.of(new ExclusiveLock(file, ch, lock));
+		} catch (Throwable t) {
+			throw closer.rethrow(t);
+		}
+
+		closer.close();
+		return Optional.absent();
+	}
+
+	public static ExclusiveLock exclusiveLock(File file) throws IOException {
+		Closer closer = Closer.create();
+		try {
+			// using RandomAccessFile, since FileOutputStream can truncate even locked file
+			final RandomAccessFile s = closer.register(new RandomAccessFile(file, "rw"));
+			final FileChannel ch = closer.register(s.getChannel());
+
+			final FileLock lock = ch.lock();
+			return new ExclusiveLock(file, ch, lock);
+		} catch (Throwable t) {
+			throw closer.rethrow(t);
+		}
+	}
+
+	public static Optional<SharedLock> trySharedLock(File file) throws IOException {
+		Closer closer = Closer.create();
+		try {
+			final RandomAccessFile s = closer.register(new RandomAccessFile(file, "rw"));
+			final FileChannel ch = closer.register(s.getChannel());
+
+			final FileLock lock = ch.tryLock(0, Long.MAX_VALUE, true);
+			if (lock != null) return Optional.of(new SharedLock(file, ch, lock));
+		} catch (Throwable t) {
+			throw closer.rethrow(t);
+		}
+
+		closer.close();
+		return Optional.absent();
+	}
+
+	public static SharedLock sharedLock(File file) throws IOException {
+		Closer closer = Closer.create();
+		try {
+			final RandomAccessFile s = closer.register(new RandomAccessFile(file, "rw"));
+			final FileChannel ch = closer.register(s.getChannel());
+
+			final FileLock lock = ch.lock(0, Long.MAX_VALUE, true);
+			return new SharedLock(file, ch, lock);
+		} catch (Throwable t) {
+			throw closer.rethrow(t);
+		}
+	}
+}
