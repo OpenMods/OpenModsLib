b3a0900c1828d3aef8d572554a7eb665b7c8006f
Bartek Bok <bartek.bok@gmail.com>
2017-06-13 22:46:33 +0200

    Tests for tool rotations

--- src/test/java/openmods/geometry/BlockRotationModeTest.java
+++ src/test/java/openmods/geometry/BlockRotationModeTest.java
@@ -1,7 +1,9 @@
 package openmods.geometry;
 
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Set;
 import net.minecraft.util.EnumFacing;
 import openmods.block.BlockRotationMode;
@@ -175,7 +177,7 @@ public class BlockRotationModeTest {
 	}
 
 	@RunWith(Parameterized.class)
-	public static class SurfacePlacementFrontStringConsistency {
+	public static class SurfacePlacementFrontStrongConsistency {
 
 		@Parameters(name = "{0}")
 		public static Iterable<BlockRotationMode> dirs() {
@@ -227,4 +229,301 @@ public class BlockRotationModeTest {
 			}
 		}
 	}
+
+	@RunWith(Parameterized.class)
+	public static class BasicToolRotations {
+
+		@Parameters(name = "{0}")
+		public static Iterable<BlockRotationMode> dirs() {
+			return Arrays.asList(
+					BlockRotationMode.TWO_DIRECTIONS,
+					BlockRotationMode.THREE_DIRECTIONS,
+					BlockRotationMode.THREE_FOUR_DIRECTIONS,
+					BlockRotationMode.FOUR_DIRECTIONS,
+					BlockRotationMode.SIX_DIRECTIONS,
+					BlockRotationMode.TWELVE_DIRECTIONS);
+		}
+
+		@Parameter
+		public BlockRotationMode mode;
+
+		@Test
+		public void toolRotationOnFrontDoesntChangeFront() {
+			for (Orientation orientation : mode.getValidDirections()) {
+				EnumFacing front = mode.getFront(orientation);
+				final Orientation rotatedOrientation = mode.calculateToolRotation(orientation, front);
+				EnumFacing rotatedFront = mode.getFront(rotatedOrientation);
+				Assert.assertEquals(front, rotatedFront);
+			}
+		}
+	}
+
+	public static void checkFrontDirectionsAfterFourRotations(BlockRotationMode mode, EnumFacing clickedSide, Orientation orientation, EnumFacing... expectedDirections) {
+		final Set<EnumFacing> results = Sets.newHashSet();
+		for (int i = 0; i < 4; i++) {
+			orientation = mode.calculateToolRotation(orientation, clickedSide);
+			Assert.assertTrue(mode.isOrientationValid(orientation));
+			results.add(mode.getFront(orientation));
+		}
+
+		Assert.assertEquals(Sets.newHashSet(expectedDirections), results);
+	}
+
+	public static void checkTopDirectionsAfterFourRotations(BlockRotationMode mode, EnumFacing clickedSide, Orientation orientation, EnumFacing... expectedDirections) {
+		final Set<EnumFacing> results = Sets.newHashSet();
+		for (int i = 0; i < 4; i++) {
+			orientation = mode.calculateToolRotation(orientation, clickedSide);
+			Assert.assertTrue(mode.isOrientationValid(orientation));
+			results.add(mode.getTop(orientation));
+		}
+
+		Assert.assertEquals(Sets.newHashSet(expectedDirections), results);
+	}
+
+	@RunWith(Parameterized.class)
+	public static class ToolRotationForWeakConsistency {
+
+		@Parameters(name = "{0}")
+		public static Iterable<BlockRotationMode> dirs() {
+			return Arrays.asList(
+					BlockRotationMode.TWO_DIRECTIONS,
+					BlockRotationMode.THREE_DIRECTIONS,
+					BlockRotationMode.THREE_FOUR_DIRECTIONS);
+		}
+
+		@Parameter
+		public BlockRotationMode mode;
+
+		@Test
+		public void toolRotationOnBackDoesntChangeFront() {
+			for (Orientation orientation : mode.getValidDirections()) {
+				final EnumFacing front = mode.getFront(orientation);
+				final EnumFacing back = front.getOpposite();
+				final Orientation rotatedOrientation = mode.calculateToolRotation(orientation, back);
+				final EnumFacing rotatedFront = mode.getFront(rotatedOrientation);
+				Assert.assertEquals(front, rotatedFront);
+			}
+		}
+	}
+
+	@RunWith(Parameterized.class)
+	public static class ToolRotationForStrongConsistency {
+
+		@Parameters(name = "{0}")
+		public static Iterable<BlockRotationMode> dirs() {
+			return Arrays.asList(
+					BlockRotationMode.FOUR_DIRECTIONS,
+					BlockRotationMode.SIX_DIRECTIONS,
+					BlockRotationMode.TWELVE_DIRECTIONS);
+		}
+
+		@Parameter
+		public BlockRotationMode mode;
+
+		@Test
+		public void toolRotationOnBackChangesFrontToOpposite() {
+			for (Orientation orientation : mode.getValidDirections()) {
+				final EnumFacing back = mode.getFront(orientation).getOpposite();
+				final Orientation rotatedOrientation = mode.calculateToolRotation(orientation, back);
+				final EnumFacing rotatedFront = mode.getFront(rotatedOrientation);
+				Assert.assertEquals(back, rotatedFront);
+			}
+		}
+	}
+
+	@RunWith(Parameterized.class)
+	public static class UniqueOrientationForEverySide {
+
+		@Parameters(name = "{0}")
+		public static Iterable<BlockRotationMode> dirs() {
+			return Arrays.asList(
+					BlockRotationMode.TWO_DIRECTIONS,
+					BlockRotationMode.THREE_DIRECTIONS,
+					BlockRotationMode.FOUR_DIRECTIONS,
+					BlockRotationMode.SIX_DIRECTIONS);
+		}
+
+		@Parameter
+		public BlockRotationMode mode;
+
+		@Test
+		public void exactlyOneOrientationForEveryFacing() {
+			final Map<EnumFacing, Orientation> stateMap = Maps.newHashMap();
+			for (Orientation orientation : mode.getValidDirections()) {
+				final EnumFacing front = mode.getFront(orientation);
+				final Orientation prev = stateMap.put(front, orientation);
+				Assert.assertNull(prev);
+			}
+		}
+
+		@Test
+		public void toolRotationOnFrontDoesntChangeOrientation() {
+			for (Orientation orientation : mode.getValidDirections()) {
+				EnumFacing front = mode.getFront(orientation);
+				final Orientation rotatedOrientation = mode.calculateToolRotation(orientation, front);
+				Assert.assertEquals(orientation, rotatedOrientation);
+			}
+		}
+	}
+
+	@RunWith(Parameterized.class)
+	public static class ExactHorizontalRotation {
+
+		@Parameters(name = "{0}")
+		public static Iterable<BlockRotationMode> dirs() {
+			return Arrays.asList(
+					BlockRotationMode.SIX_DIRECTIONS,
+					BlockRotationMode.TWELVE_DIRECTIONS);
+		}
+
+		@Parameter
+		public BlockRotationMode mode;
+
+		@Test
+		public void toolRotationOnHorizontalsChangesFrontToClickedSide() {
+			for (Orientation initialOrientation : mode.getValidDirections()) {
+				for (EnumFacing rotatedSide : EnumFacing.HORIZONTALS) {
+					Orientation orientation = initialOrientation;
+					final Set<Orientation> results = Sets.newHashSet();
+					for (int i = 0; i < 4; i++) {
+						orientation = mode.calculateToolRotation(orientation, rotatedSide);
+						Assert.assertTrue(mode.isOrientationValid(orientation));
+						Assert.assertTrue(mode.getFront(orientation) == rotatedSide);
+						results.add(orientation);
+					}
+
+					Assert.assertTrue("Multiple states: " + results, results.size() == 1);
+				}
+			}
+		}
+	}
+
+	public static class TwoDirections {
+
+		private static final BlockRotationMode MODE = BlockRotationMode.TWO_DIRECTIONS;
+
+		@Test
+		public void toolRotationOnHorizontalChangesFrontAxisToClickedSide() {
+			for (Orientation orientation : MODE.getValidDirections()) {
+				for (EnumFacing rotatedSide : EnumFacing.HORIZONTALS) {
+					final Orientation rotatedOrientation = MODE.calculateToolRotation(orientation, rotatedSide);
+					final EnumFacing rotatedFront = MODE.getFront(rotatedOrientation);
+					Assert.assertEquals(rotatedSide.getAxis(), rotatedFront.getAxis());
+				}
+			}
+		}
+
+		@Test
+		public void toolTopRotationCoversHorizontals() {
+			checkFrontDirectionsAfterFourRotations(MODE, EnumFacing.UP, Orientation.XP_YP, EnumFacing.NORTH, EnumFacing.WEST);
+		}
+
+		@Test
+		public void toolBottomRotationCoversHorizontals() {
+			checkFrontDirectionsAfterFourRotations(MODE, EnumFacing.DOWN, Orientation.XP_YP, EnumFacing.NORTH, EnumFacing.WEST);
+		}
+	}
+
+	public static class ThreeDirections {
+
+		private static final BlockRotationMode MODE = BlockRotationMode.THREE_DIRECTIONS;
+
+		@Test
+		public void toolRotationOnAnySideChangesFrontAxisToClickedSide() {
+			for (Orientation orientation : MODE.getValidDirections()) {
+				for (EnumFacing rotatedSide : EnumFacing.VALUES) {
+					final Orientation rotatedOrientation = MODE.calculateToolRotation(orientation, rotatedSide);
+					final EnumFacing rotatedFront = MODE.getFront(rotatedOrientation);
+					Assert.assertEquals(rotatedSide.getAxis(), rotatedFront.getAxis());
+				}
+			}
+		}
+	}
+
+	public static class FourDirections {
+		private static final BlockRotationMode MODE = BlockRotationMode.FOUR_DIRECTIONS;
+
+		@Test
+		public void toolTopRotationCoversHorizontals() {
+			checkFrontDirectionsAfterFourRotations(MODE, EnumFacing.UP, Orientation.XP_YP, EnumFacing.NORTH, EnumFacing.SOUTH, EnumFacing.WEST, EnumFacing.EAST);
+		}
+
+		@Test
+		public void toolBottomRotationCoversHorizontals() {
+			checkFrontDirectionsAfterFourRotations(MODE, EnumFacing.DOWN, Orientation.XP_YP, EnumFacing.NORTH, EnumFacing.SOUTH, EnumFacing.WEST, EnumFacing.EAST);
+		}
+	}
+
+	public static class SixDirections {
+
+		private static final BlockRotationMode MODE = BlockRotationMode.SIX_DIRECTIONS;
+
+		@Test
+		public void toolRotationOnAnySideChangesFrontToClickedSide() {
+			for (Orientation orientation : MODE.getValidDirections()) {
+				for (EnumFacing rotatedSide : EnumFacing.VALUES) {
+					checkFrontDirectionsAfterFourRotations(MODE, rotatedSide, orientation, rotatedSide);
+				}
+			}
+		}
+	}
+
+	public static class ThreeFourDirections {
+
+		private static final BlockRotationMode MODE = BlockRotationMode.THREE_FOUR_DIRECTIONS;
+
+		@Test
+		public void toolRotationOnTopCoversHorizontals() {
+			checkTopDirectionsAfterFourRotations(MODE, EnumFacing.UP, Orientation.XP_YP, EnumFacing.HORIZONTALS);
+			checkFrontDirectionsAfterFourRotations(MODE, EnumFacing.UP, Orientation.XP_YP, EnumFacing.UP);
+		}
+
+		@Test
+		public void toolRotationOnBottomCoversHorizontals() {
+			checkTopDirectionsAfterFourRotations(MODE, EnumFacing.DOWN, Orientation.XP_YP, EnumFacing.HORIZONTALS);
+			checkFrontDirectionsAfterFourRotations(MODE, EnumFacing.DOWN, Orientation.XP_YP, EnumFacing.UP);
+		}
+
+		@Test
+		public void toolRotationOnEastCoversFourSides() {
+			checkTopDirectionsAfterFourRotations(MODE, EnumFacing.EAST, Orientation.XP_YP, EnumFacing.NORTH, EnumFacing.SOUTH, EnumFacing.UP, EnumFacing.DOWN);
+			checkFrontDirectionsAfterFourRotations(MODE, EnumFacing.EAST, Orientation.XP_YP, EnumFacing.WEST);
+		}
+
+		@Test
+		public void toolRotationOnWestCoversFourSides() {
+			checkTopDirectionsAfterFourRotations(MODE, EnumFacing.WEST, Orientation.XP_YP, EnumFacing.NORTH, EnumFacing.SOUTH, EnumFacing.UP, EnumFacing.DOWN);
+			checkFrontDirectionsAfterFourRotations(MODE, EnumFacing.WEST, Orientation.XP_YP, EnumFacing.WEST);
+		}
+
+		@Test
+		public void toolRotationOnNorthCoversFourSides() {
+			checkTopDirectionsAfterFourRotations(MODE, EnumFacing.NORTH, Orientation.XP_YP, EnumFacing.EAST, EnumFacing.WEST, EnumFacing.UP, EnumFacing.DOWN);
+			checkFrontDirectionsAfterFourRotations(MODE, EnumFacing.NORTH, Orientation.XP_YP, EnumFacing.NORTH);
+		}
+
+		@Test
+		public void toolRotationOnSouthCoversFourSides() {
+			checkTopDirectionsAfterFourRotations(MODE, EnumFacing.SOUTH, Orientation.XP_YP, EnumFacing.EAST, EnumFacing.WEST, EnumFacing.UP, EnumFacing.DOWN);
+			checkFrontDirectionsAfterFourRotations(MODE, EnumFacing.SOUTH, Orientation.XP_YP, EnumFacing.NORTH);
+		}
+	}
+
+	public static class TwelveDirections {
+
+		private static final BlockRotationMode MODE = BlockRotationMode.TWELVE_DIRECTIONS;
+
+		@Test
+		public void toolRotationOnTopCoversHorizontals() {
+			checkTopDirectionsAfterFourRotations(MODE, EnumFacing.UP, Orientation.XP_YP, EnumFacing.HORIZONTALS);
+			checkFrontDirectionsAfterFourRotations(MODE, EnumFacing.UP, Orientation.XP_YP, EnumFacing.UP);
+		}
+
+		@Test
+		public void toolRotationOnBottomCoversHorizontals() {
+			checkTopDirectionsAfterFourRotations(MODE, EnumFacing.DOWN, Orientation.XP_YP, EnumFacing.HORIZONTALS);
+			checkFrontDirectionsAfterFourRotations(MODE, EnumFacing.DOWN, Orientation.XP_YP, EnumFacing.DOWN);
+		}
+	}
+
 }
