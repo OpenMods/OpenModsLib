ba83ce3a992b20f00787d78e04f4b3cfbda22dff
Bartek Bok <bartek.bok@gmail.com>
2017-06-22 18:10:41 +0200

    Few utilities for colored blocks

---
+++ src/main/java/openmods/model/ModelUtils.java
@@ -0,0 +1,29 @@
+package openmods.model;
+
+import net.minecraft.client.renderer.ItemMeshDefinition;
+import net.minecraft.client.renderer.block.model.ModelBakery;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.model.ModelLoader;
+
+public class ModelUtils {
+
+	public static void registerMetaInsensitiveModel(Item item) {
+		registerMetaInsensitiveModel(item, "inventory");
+	}
+
+	public static void registerMetaInsensitiveModel(Item item, String variant) {
+		final ModelResourceLocation location = new ModelResourceLocation(item.getRegistryName(), variant);
+
+		ModelBakery.registerItemVariants(item, location);
+
+		ModelLoader.setCustomMeshDefinition(item, new ItemMeshDefinition() {
+			@Override
+			public ModelResourceLocation getModelLocation(ItemStack stack) {
+				return location;
+			}
+		});
+	}
+
+}
