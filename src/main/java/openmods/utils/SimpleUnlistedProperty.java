d66b570cc9e7950e35d6083dc90e0ca1c4919b53
Bartek Bok <bartek.bok@gmail.com>
2017-07-20 17:05:11 +0200

    Utility class: simple unlisted property

---
+++ src/main/java/openmods/utils/SimpleUnlistedProperty.java
@@ -0,0 +1,39 @@
+package openmods.utils;
+
+import net.minecraftforge.common.property.IUnlistedProperty;
+
+public class SimpleUnlistedProperty<T> implements IUnlistedProperty<T> {
+
+	private final Class<T> type;
+	private final String name;
+
+	public SimpleUnlistedProperty(Class<T> type, String name) {
+		this.type = type;
+		this.name = name;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public boolean isValid(T value) {
+		return true;
+	}
+
+	@Override
+	public Class<T> getType() {
+		return type;
+	}
+
+	@Override
+	public String valueToString(T value) {
+		return value.toString();
+	}
+
+	public static <T> IUnlistedProperty<T> create(Class<T> type, String name) {
+		return new SimpleUnlistedProperty<T>(type, name);
+	}
+
+}
