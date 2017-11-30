df660223573faa5e37c6eb995ae13414635066f9
Bartek Bok <bartek.bok@gmail.com>
2017-11-30 19:49:55 +0100

    Extends custom recipe base

--- src/main/java/openmods/utils/CustomRecipeBase.java
+++ src/main/java/openmods/utils/CustomRecipeBase.java
@@ -5,9 +5,26 @@ import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.registries.IForgeRegistryEntry;
 
-public abstract class CustomRecipeBase implements IRecipe {
+public abstract class CustomRecipeBase extends IForgeRegistryEntry.Impl<IRecipe> implements IRecipe {
+
+	private final String group;
+
+	public CustomRecipeBase(String group) {
+		this.group = group;
+	}
+
+	public CustomRecipeBase(ResourceLocation group) {
+		this.group = group.toString();
+	}
+
+	@Override
+	public String getGroup() {
+		return group;
+	}
 
 	@Override
 	@Nonnull
@@ -25,6 +42,11 @@ public abstract class CustomRecipeBase implements IRecipe {
 		}
 
 		return result;
+	}
+
+	@Override
+	public boolean isDynamic() {
+		return true;
 	}
 
 }
