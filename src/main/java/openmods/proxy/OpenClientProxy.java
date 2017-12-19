820e9703187358a920a15bd26505d994e8347ba9
Bartek Bok <bartek.bok@gmail.com>
2017-12-19 21:03:09 +0100

    Add custom perspective-aware model

--- src/main/java/openmods/proxy/OpenClientProxy.java
+++ src/main/java/openmods/proxy/OpenClientProxy.java
@@ -40,6 +40,7 @@ import openmods.gui.ClientGuiHandler;
 import openmods.model.MappedModelLoader;
 import openmods.model.ModelWithDependencies;
 import openmods.model.MultiLayerModel;
+import openmods.model.PerspectiveAwareModel;
 import openmods.model.eval.EvalExpandModel;
 import openmods.model.eval.EvalModel;
 import openmods.model.itemstate.ItemStateModel;
@@ -132,6 +133,7 @@ public final class OpenClientProxy implements IOpenModsProxy {
 				.put("stateitem", ItemStateModel.EMPTY)
 				.put("eval", EvalModel.EMPTY)
 				.put("eval-expand", EvalExpandModel.EMPTY)
+				.put("perspective-aware", PerspectiveAwareModel.EMPTY)
 				.build(OpenMods.MODID));
 
 		((IReloadableResourceManager)Minecraft.getMinecraft().getResourceManager()).registerReloadListener(hitboxManager);
