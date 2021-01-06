1282230b26c26d9eb9138efa915f39d035825beb
Bartek Bok <bartek.bok@gmail.com>
2021-01-06 13:00:40 +0100

    Fixes for elevator

---
+++ src/main/java/openmods/utils/FieldsAreNonnullByDefault.java
@@ -0,0 +1,13 @@
+package openmods.utils;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import javax.annotation.Nonnull;
+import javax.annotation.meta.TypeQualifierDefault;
+
+@Nonnull
+@TypeQualifierDefault(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FieldsAreNonnullByDefault {
+}
